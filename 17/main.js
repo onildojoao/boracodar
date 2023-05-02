dc16a4bbcdb1e16ff2c1d80a35c6aa2c8a7aed5e
Onildo João <14207982+onildojoao@users.noreply.github.com>
2023-05-02 16:42:53 -0300

    Update modal opening

--- 17/main.js
+++ 17/main.js
@@ -2,9 +2,24 @@ const yearModal = document.querySelector(".modal-year")
 const filterButton = document.querySelector(".filter")
 const closeModalButton = document.querySelector(".modal-year .close")
 
-filterButton.addEventListener("click", toggleYearModal)
-closeModalButton.addEventListener("click", toggleYearModal)
+filterButton.addEventListener("click", showYearModal)
 
-function toggleYearModal() {
-  yearModal.classList.toggle("hidden")
+yearModal.addEventListener("click", (e) => {
+  const dialogDimensions = yearModal.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    yearModal.close()
+  }
+})
+
+function showYearModal() {
+  yearModal.showModal()
+}
+
+function closeYearModal() {
+  yearModal.close()
 }
