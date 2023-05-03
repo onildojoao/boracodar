be09a587fffa38659a3c9aaa76645f3bc0f1b13a
Onildo João <14207982+onildojoao@users.noreply.github.com>
2023-05-03 13:10:01 -0300

    Dinamicaly create months and day divs with js

--- 17/main.js
+++ 17/main.js
@@ -1,8 +1,111 @@
+const months = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+]
+const weekDays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+const days = [
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+]
+const currentYear = new Date().getFullYear()
+const currentMonth = new Date().getMonth()
 const yearModal = document.querySelector(".modal-year")
 const filterButton = document.querySelector(".filter")
-const closeModalButton = document.querySelector(".modal-year .close")
+const monthsElement = document.querySelector(".modal-year main")
+const daysElement = document.querySelector(".modal-month main")
+const weekDaysElement = document.querySelector(".modal-month header")
+const currentYearElement = document.querySelector(".modal-year span")
+const currentMonthElement = document.querySelector(".modal-month span")
 
 filterButton.addEventListener("click", showYearModal)
+monthsElement.innerHTML = populateMonths()
+daysElement.innerHTML = populateDays()
+weekDaysElement.innerHTML = populateWeekDays()
+
+currentYearElement.innerHTML = currentYear
+currentMonthElement.innerHTML = `${months[currentMonth]} ${currentYear}`
+
+function populateMonths() {
+  let output = ""
+
+  const currentMonth = months[new Date().getMonth()]
+  for (let month of months) {
+    const selected = currentMonth == month ? "selected relative" : ""
+    output = output + ` <div class='${selected}'>${month}</div>`
+  }
+  return output
+}
+
+function populateDays() {
+  let output = ""
+  /* const currentDay = days[new Date().getDay()] */
+for (let day of days){
+    /* const selected = currentDay == days[i] ? "selected relative" : "" */
+    output = output + `<div class="">${day}</div>`
+}
+
+  return output
+}
+
+function populateWeekDays() {
+  let output = ""
+  for (let wekD of weekDays) {
+    console.log(weekDays)
+    console.log(wekD)
+    output = output + `<div>${wekD}</div>`
+  }
+  return output
+}
 
 yearModal.addEventListener("click", (e) => {
   const dialogDimensions = yearModal.getBoundingClientRect()
